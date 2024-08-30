2efced92a5b30d04713f74daa584968ffd286203
Chethan A C <acchethan15@gmail.com>
2024-08-30 23:07:40 +0530

    Create Commands.js

---
+++ Commands.js
@@ -0,0 +1,9 @@
+Node js
+node -v //gives info about the node version on your system
+node //to get start
+node index.js //import the functioing file of js
+
+
+Native Modules
+npm init //initialisation of node package manager
+npm install <something>//syntax
